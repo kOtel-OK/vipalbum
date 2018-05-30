6d242d25911ed9ac301058d910f2e5e65594adbd
Roman Kotelnykov <sunrise39@yandex.ru>
2018-05-30 17:34:01 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,23 @@
+//mocha.setup('bdd');
+//
+//const assert = chai.assert;
+//
+//const validateMail = (mail) => {
+//	const reference = new RegExp(/^\w+@\w+\.\w{1,3}$/);
+//	if (reference.test(mail)) {
+//		return true;
+//	} else {
+//		return false;
+//	}
+//};
+//
+//
+//
+//describe('Mail test', () => {
+//	it('mail should be correct', () => {
+//		assert(validateMail('qqq@sqq.aaa'));
+//	})
+//});
+//
+//mocha.run();
+
